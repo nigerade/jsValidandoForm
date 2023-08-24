384ecaca9ede1e5903886e8b9d6be360f0da3b22
Nicholas Gerade <nicholasgerade@gmail.com>
2023-08-23 23:18:48 -0300

    alteracoes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import ehUmCPF from "./valida-cpf";
+import ehMaiorDeIdade from "./valida-idade";
 
 const camposDeFormulario = document.querySelectorAll("[required]");
 
@@ -9,5 +10,9 @@ camposDeFormulario.forEach((campo) => {
 function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
+    }
+
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
     }
 }
